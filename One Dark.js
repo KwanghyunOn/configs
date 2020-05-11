cb74f3fb1901e9ecb8f5b0affbef9b5af2a96c87
KwanghyunOn <kwanghyun.on@gmail.com>
2020-05-11 14:50:44 +0900

    Add one dark theme for blink shell

---
+++ One Dark.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#272c33", "#e26a72", "#96c475", "#d29a61", "#5cadf1", "#c774df", "#51b5c3", "#abb1bf", "#272c33", "#e26a72", "#96c475", "#d29a61", "#5cadf1", "#c774df", "#51b5c3", "#abb1bf"]);
+t.prefs_.set('foreground-color', "#abb1bf");
+t.prefs_.set('background-color', "#272c33");
+t.prefs_.set('cursor-color', 'rgba(79,135,255,0.5)');
